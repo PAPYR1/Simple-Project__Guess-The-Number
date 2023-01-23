58ec11a704286ddf577f087bfb3b35f8b530b750
PAPYR1 <mitkotf2@gmail.com>
2023-01-23 22:24:25 +0200

    Update :D

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Random RNG = new Random();
+int ComputerNumber = RNG.Next(1, 101);
+while (true)
+{
+    Console.Write("Guess a number (1-100): ");
+    string PlayerGuess = Console.ReadLine();
+    bool isValid = int.TryParse(PlayerGuess, out int playerNumber);
+    if (isValid)
+    {
+        if (playerNumber == ComputerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (playerNumber > ComputerNumber)
+        {
+            Console.WriteLine("Too High");
+        }
+        else if (playerNumber < ComputerNumber)
+        {
+            Console.WriteLine("Too Low");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid Input.");
+    }
+}
